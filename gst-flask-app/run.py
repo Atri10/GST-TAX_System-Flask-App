ea93af28a211f7cf85cf3847f5439792c26f60c6
Atri Patel <64589033+Atri10@users.noreply.github.com>
2021-09-25 18:12:50 +0530

    Update run.py

--- gst-flask-app/run.py
+++ gst-flask-app/run.py
@@ -1,7 +1,6 @@
 from GST_API_BackEnd import GST_API
 
 if __name__ == '__main__':
-    GST_API.run(debug=False, host="0.0.0.0", port="3000")
-
+    GST_API.run(debug=False, host="192.168.0.106", port="5000")
 
 
